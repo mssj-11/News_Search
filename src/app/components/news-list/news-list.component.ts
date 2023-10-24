f587d38f2f41f1f6d71a96db9cd65620c8b1afaa
mssj-11 <michael11.salgado10@gmail.com>
2023-10-23 21:24:58 -0600

    List

--- src/app/components/news-list/news-list.component.ts
+++ src/app/components/news-list/news-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
   styleUrls: ['./news-list.component.css']
 })
-export class NewsListComponent {
+export class NewsListComponent implements OnInit{
+  @Input() newsList: any;
+
+  contructor() {  }
+
+  ngOnInit(): void {  }
 
 }
