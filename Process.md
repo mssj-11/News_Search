a7e0eab712e04ee2279e23f20e1bc5aec84a6e3f
mssj-11 <michael11.salgado10@gmail.com>
2023-10-23 18:48:10 -0600

    Components and Service

--- Process.md
+++ Process.md
@@ -15,3 +15,27 @@ npm install
 ```sh
 ng serve --o
 ```
+
+
+##  Components
+### In Components
+```sh
+ng g c components/search-form
+```
+```sh
+ng g c components/news-list
+```
+```sh
+ng g c components/navbar
+```
+### In Shared
+```sh
+ng g c shared/spinner
+```
+### In Services
+```sh
+ng g s services/news
+```
+```sh
+ng g s services/
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { NewsListComponent } from './components/news-list/news-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    SearchFormComponent,
+    NewsListComponent,
+    NavbarComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
